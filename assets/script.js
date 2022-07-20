da897c96ce9e3d0353c5c0344c961974c8ce8f06
Jacob Canepa <jacobmcanepa@gmail.com>
2022-07-19 21:06:22 -0700

    can now generate random password

--- assets/script.js
+++ assets/script.js
@@ -3,45 +3,65 @@ var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var numbers = "0123456789"
 var special = "!#$&%;()'*+-/:<>=?@^_`{|}.~"
+var available = "";
 
 // generate password function
 var generatePassword = function() {
-  var available = [];
-
   var includeUppercase = window.confirm("Would you like to include uppercase characters in your password?");
 
   if (includeUppercase) {
-    available.push(uppercase);
+    available += uppercase;
   }
 
   var includeLowercase = window.confirm("Would you like to include lowercase characters in your password?");
 
   if (includeLowercase) {
-    available.push(lowercase);
+    available += lowercase;
   }
 
   var includeNumbers = window.confirm("Would you like to include numbers in your password?");
 
   if (includeNumbers) {
-    available.push(numbers);
+    available += numbers;
   }
 
   var includeSpecial = window.confirm("Would you like to include special characters in your password?");
 
   if (includeSpecial) {
-    available.push(special);
+    available += special;
   }
 
-  var characterAmount = window.prompt("How many characters would you like your password to be? Min: 8 Max: 128")
-
-  var randomize = function(length) {
-    for (var i =0; i < length; i++) {
-      console.log(available[i%available.length]);
-    }
-  }
-
-  randomize(parseInt(characterAmount));
+  finalPassword = randomize(characterAmount());
+  return finalPassword;
 };
+
+var characterAmount = function() {
+  var value = prompt("How many characters would you like you password to be? Min: 8 Max: 128");
+  
+  if (parseInt(value) < 8) {
+    alert("Your password must be more than 8 characters.");
+    characterAmount();
+  }
+
+  if (parseInt(value) > 128) {
+    alert("Your password must be less than 128 characters.");
+    characterAmount();
+  }
+
+  else {
+    return parseInt(value);
+  }
+};
+
+var randomize = function(length) {
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    password += available.charAt(Math.floor(Math.random() * available.length));
+  }
+
+  return password;
+};
+
 
 
 // Get references to the #generate element
